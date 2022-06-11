adf86d630386be19c0e90ebc7a2a2aef723c4c13
Viktor Denikin <leshden@yandex.ru>
2022-06-11 18:33:36 +0300

    handle input text

--- src/components/enter-panel/EnterPanel.js
+++ src/components/enter-panel/EnterPanel.js
@@ -1,12 +1,27 @@
 import './EnterPanel.css'
+import {useState} from 'react';
 
 const EnterPanel = () => {
+  const [inputText, setInputText] = useState('');
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      console.log(`Enter key, value ${inputText}`);
+      setInputText('');
+    }
+  }
+
+  const onChangeInputText = (e) => {
+      setInputText(e.target.value);
+  }
+
   return (
     <div className='enter-todos'>
       <div className='down-pointing-angle-container-todo'>
         <div className='down-pointing-angle-todo'>&#x2771;</div>
       </div>
-      <input type="text" className='enter-text-todos' placeholder='What needs to be done?' />
+      <input type="text" className='enter-text-todos' value = {inputText} onChange={onChangeInputText}
+             placeholder='What needs to be done? And Enter' onKeyDown={handleKeyDown} />
     </div>
   );
 }
