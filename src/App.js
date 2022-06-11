ff18feb89a46383f7318f637f1dfb53d55357542
Viktor Denikin <leshden@yandex.ru>
2022-06-11 17:56:06 +0300

    item event component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import EnterPanel from './components/enter-panel/EnterPanel'
 import FooterPanel from './components/footer-panel/FooterPanel';
+import ItemEvent from './components/item-event/ItemEvent';
 
 function App() {
   return (
     <div className="App">
         <div className='title-todos'> todos </div>
         <EnterPanel />
+        <ItemEvent />
         <FooterPanel />
     </div>
   );
---
+++ src/components/item-event/ItemEvent.js
@@ -0,0 +1,14 @@
+import './ItemEvent.css';
+
+const ItemEvent = () => {
+  return (
+    <div className='item-container-todos'>
+      <div className='item-mark-container-todo'>
+        <div className='item-mark-todo'></div>
+      </div>
+      <div className='item-text-todos'> Тестовое задание </div>
+    </div>
+  );
+}
+
+export default ItemEvent;
